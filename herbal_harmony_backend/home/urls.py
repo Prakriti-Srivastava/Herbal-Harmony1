41da543128c3c6794e0d7d74aa2dce0e3ea49ba8
Riya Singh <riya.42005.singh@gmail.com>
2025-09-17 00:50:53 +0530

    yoga section changed

--- herbal_harmony_backend/home/urls.py
+++ herbal_harmony_backend/home/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from home import views
+from . import views
 
 urlpatterns = [
-   path("", views.index, name='home'),
+   path('', views.index, name='home'),
    path("about", views.about, name='about'),
+   path("yoga", views.yoga, name='yoga'),
+   path("index", views.index, name='index'),
 ]
--- herbal_harmony_backend/home/views.py
+++ herbal_harmony_backend/home/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, HttpResponse
+import re
+from django.utils.html import escape
+from django.utils.safestring import mark_safe
 
 # Create your views here.
 def index(request):
@@ -7,3 +10,26 @@ def index(request):
 
 def about(request):
     return HttpResponse('This is the about page of Herbal Harmony Backend')
+
+def yoga(request):
+    query = request.GET.get('query', '').strip()
+
+    content = """
+    """
+
+    highlighted_content = content  # Default to original content if no query
+    if query:
+        # Escape the query to prevent XSS attacks
+        #escaped_query = escape(query)
+        # Use regex to find whole word matches, case insensitive
+        pattern = re.compile(r'\b' + re.escape(query) + r'\b', re.IGNORECASE)
+        # Replace matches with highlighted version
+        highlighted_content = pattern.sub(
+            lambda m: f"<mark>{escape(m.group(0))}</mark>", content
+            )
+        highlighted_content = mark_safe(highlighted_content)
+
+    return render(request, 'yoga.html', {
+        'content': highlighted_content,
+        'query': query
+        })
