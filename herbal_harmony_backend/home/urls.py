cf9bb293493167697e9c35350c07f4320fea014a
Riya Singh <riya.42005.singh@gmail.com>
2025-09-17 15:17:19 +0530

    home_backend

--- herbal_harmony_backend/home/urls.py
+++ herbal_harmony_backend/home/urls.py
@@ -4,8 +4,11 @@ from home import views
 from . import views
 
 urlpatterns = [
-   path('', views.index, name='home'),
-   path("about", views.about, name='about'),
+   #path('', views.index, name='home'),
+  # path("about", views.about, name='about'),
    path("yoga", views.yoga, name='yoga'),
-   path("index", views.index, name='index'),
+   #path("index", views.index, name='index'),
+   path('', views.Home, name='Home'),
+   path('About', views.About, name='About'),
+   
 ]
--- herbal_harmony_backend/home/views.py
+++ herbal_harmony_backend/home/views.py
@@ -4,17 +4,23 @@ from django.utils.html import escape
 from django.utils.safestring import mark_safe
 
 # Create your views here.
-def index(request):
-    return render(request, 'index.html')
+#def index(request):
+    #return render(request, 'index.html')
     #return HttpResponse('This is the home page of Herbal Harmony Backend')
 
-def about(request):
-    return HttpResponse('This is the about page of Herbal Harmony Backend')
+#def about(request):
+    #return HttpResponse('This is the about page of Herbal Harmony Backend')
 
 def yoga(request):
     query = request.GET.get('query', '').strip()
 
     content = """
+                Yoga is the journey of the self,
+                   through the self,
+                          to the self."
+
+                          - The Bhagavad Gita
+
     """
 
     highlighted_content = content  # Default to original content if no query
@@ -33,3 +39,10 @@ def yoga(request):
         'content': highlighted_content,
         'query': query
         })
+
+def Home(request):
+    return render(request, 'Home.html')
+
+def About(request):
+    return render(request, 'About.html')
+
