5fae9e6a5b953403c73a79497da51c88119eacda
Riya Singh <riya.42005.singh@gmail.com>
2025-09-18 01:01:11 +0530

    to much work

--- herbal_harmony_backend/home/views.py
+++ herbal_harmony_backend/home/views.py
@@ -11,38 +11,14 @@ from django.utils.safestring import mark_safe
 #def about(request):
     #return HttpResponse('This is the about page of Herbal Harmony Backend')
 
-def yoga(request):
-    query = request.GET.get('query', '').strip()
 
-    content = """
-                Yoga is the journey of the self,
-                   through the self,
-                          to the self."
-
-                          - The Bhagavad Gita
-
-    """
-
-    highlighted_content = content  # Default to original content if no query
-    if query:
-        # Escape the query to prevent XSS attacks
-        #escaped_query = escape(query)
-        # Use regex to find whole word matches, case insensitive
-        pattern = re.compile(r'\b' + re.escape(query) + r'\b', re.IGNORECASE)
-        # Replace matches with highlighted version
-        highlighted_content = pattern.sub(
-            lambda m: f"<mark>{escape(m.group(0))}</mark>", content
-            )
-        highlighted_content = mark_safe(highlighted_content)
-
-    return render(request, 'yoga.html', {
-        'content': highlighted_content,
-        'query': query
-        })
 
 def Home(request):
     return render(request, 'Home.html')
 
 def About(request):
     return render(request, 'About.html')
+
+def yoga(request):
+    return render(request, 'yoga.html')
 
